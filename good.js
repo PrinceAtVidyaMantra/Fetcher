7f62175bd56aba4b680b8dc4767bf3761fd8cb61
Prince Kunwar <Prince.Kunwar@VidyaMantra.com>
2022-02-19 23:00:25 +0530

    done

--- good.js
+++ good.js
@@ -1,6 +1,4 @@
-const startDate = new Date(2022, 0, 1);
-
-// let lastFetch = startDate;
+const startDate = new Date(Date.UTC(2022, 0, 1));
 
 const MONTH_LIST = [
     'January', 'February', 'March', 'April', 'May', 'June',
@@ -8,7 +6,7 @@ const MONTH_LIST = [
 ];
 
 function findDayOfYear(now) {
-    const start = new Date(now.getUTCFullYear(), 0, 0);
+    const start = new Date(Date.UTC(now.getUTCFullYear(), 0, 0));
     const diff = now - start;
     const oneDay = 1000 * 60 * 60 * 24;
     return Math.floor(diff / oneDay);
@@ -36,10 +34,10 @@ let request = (interval_name, date) => {
     }
     else if (interval_name == 'hour') {
         const filename = `/h/${year}-${dayOfYear}-${hours + 1}.json`;
-        console.log(`Time: Hour ${hours} of day ${dayOfYear}, ${year}, File: ${filename}`);
+        console.log(`Time: Hour ${hours + 1} of day ${dayOfYear}, ${year}, File: ${filename}`);
     }
     else {
-        console.log('none');
+        console.log('Unknown case', interval_name, 'found');
     }
 };
 
@@ -47,18 +45,11 @@ function daysInMonth(month, year) {
     return new Date(year, month, 0).getUTCDate();
 }
 
-function initialSync(currentDate) {
-    console.log(currentDate);
-    let dateItr = startDate;
-    console.log(dateItr);
-    
+function forward(dateItr, currentDate) {
     while (dateItr.getUTCFullYear() < currentDate.getUTCFullYear() && dateItr < currentDate) {
         request('year', dateItr);
         dateItr.setUTCFullYear(dateItr.getUTCFullYear() + 1);
-        console.log(dateItr);
     }
-    // console.log(dateItr);
-    // Fetch Next
     while (dateItr.getUTCMonth() < currentDate.getUTCMonth() && dateItr < currentDate) {
         request('month', dateItr);
         dateItr.setUTCMonth(dateItr.getUTCMonth() + 1);
@@ -73,7 +64,15 @@ function initialSync(currentDate) {
     }
 }
 
+function initialSync(currentDate) {
+    console.log(`Fetching records from ${startDate.toUTCString()} to ${currentDate.toUTCString()}`);
+    let dateItr = startDate;
+    forward(dateItr, currentDate);
+}
+
 function sync(lastFetch, currentDate) {
+    console.log(`Fetching records from ${lastFetch.toUTCString()} to ${currentDate.toUTCString()}`);
+
     let dateItr = lastFetch; // last date when fetched
 
     // Fetch Upto this year
@@ -98,27 +97,7 @@ function sync(lastFetch, currentDate) {
         if (dateItr.getUTCMonth() != months) break;
     }
 
-    while (dateItr.getUTCFullYear() < currentDate.getUTCFullYear() && dateItr < currentDate) {
-        request('year', dateItr);
-        dateItr.setUTCFullYear(dateItr.getUTCFullYear() + 1);
-    }
-
-    // Fetch Next
-    while (dateItr.getUTCMonth() < currentDate.getUTCMonth() && dateItr < currentDate) {
-        request('month', dateItr);
-        dateItr.setUTCMonth(dateItr.getUTCMonth() + 1);
-    }
-    while (dateItr.getUTCDate() < currentDate.getUTCDate() && dateItr < currentDate) {
-        request('day', dateItr);
-        dateItr.setUTCDate(dateItr.getUTCDate() + 1);
-    }
-    while (dateItr.getUTCHours() < currentDate.getUTCHours() && dateItr < currentDate) {
-        request('hour', dateItr);
-        dateItr.setUTCHours(dateItr.getUTCHours() + 1);
-    }
+    forward(dateItr, currentDate);
 }
 
-const currentDate = new Date(2023, 2, 4);
-
-// sync(currentDate)
-// initialSync(currentDate)
+// const currentDate = new Date(2023, 2, 4);
